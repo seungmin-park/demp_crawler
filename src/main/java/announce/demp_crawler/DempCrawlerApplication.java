94573c52a436509d4c259f91b3f44402b42a0149
seungmin-park <tmddudals369@naver.com>
2022-05-19 20:57:54 +0900

    create project

---
+++ src/main/java/announce/demp_crawler/DempCrawlerApplication.java
@@ -0,0 +1,13 @@
+package announce.demp_crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DempCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DempCrawlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/announce/demp_crawler/DempCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package announce.demp_crawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DempCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
